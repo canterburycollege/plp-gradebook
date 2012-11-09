87aa20a56b686843cc7c3c711e82f7956371c941
Russell Hardy <r.hardy@canterburycollege.ac.uk>
2012-11-09 12:39:37 -0200

    test new branch to add gradebook column ... again 2

--- plp-gradebook/actions/view_studentlist.ajax.php
+++ plp-gradebook/actions/view_studentlist.ajax.php
@@ -212,7 +212,9 @@ if (!empty($studentslist)) {
         
         
         $data['view'] = "<a href='{$CFG->wwwroot}/blocks/ilp/actions/view_main.php?user_id={$student->id}{$course_param}' >" . get_string('viewplp', 'block_ilp') . "</a>";
-/* test sync */
+        
+        // @test - add link to list of course gradebooks for student
+        $data['grades'] = "<a href='#'>blank url</a>";
         
 		//we will only attempt to get MIS data if an attendace plugin has been selected in the settings page
 		
