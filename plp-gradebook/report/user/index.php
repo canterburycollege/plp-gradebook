0fc865179a8ffd883d96b25c4c3d42b5d1df4775
Russell Hardy <r.hardy@canterburycollege.ac.uk>
2012-12-19 09:31:57 -0200

    remove user selector

--- plp-gradebook/report/user/index.php
+++ plp-gradebook/report/user/index.php
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-require_once '../../../config.php';
+require_once '../../../../config.php';
 require_once $CFG->libdir.'/gradelib.php';
 require_once $CFG->dirroot.'/grade/lib.php';
 require_once $CFG->dirroot.'/grade/report/user/lib.php';
@@ -23,7 +23,7 @@ require_once $CFG->dirroot.'/grade/report/user/lib.php';
 $courseid = required_param('id', PARAM_INT);
 $userid   = optional_param('userid', $USER->id, PARAM_INT);
 
-$PAGE->set_url(new moodle_url('/grade/report/user/index.php', array('id'=>$courseid)));
+$PAGE->set_url(new moodle_url('/blocks/ilp/grade/report/user/index.php', array('id'=>$courseid)));
 
 /// basic access checks
 if (!$course = $DB->get_record('course', array('id' => $courseid))) {
@@ -125,12 +125,6 @@ if (has_capability('moodle/grade:viewall', $context)) { //Teachers will see all 
         $studentnamelink = html_writer::link(new moodle_url('/user/view.php', array('id' => $report->user->id, 'course' => $courseid)), fullname($report->user));
         print_grade_page_head($courseid, 'report', 'user', get_string('pluginname', 'gradereport_user') . ' - ' . $studentnamelink);
         groups_print_course_menu($course, $gpr->get_return_url('index.php?id='.$courseid, array('userid'=>0)));
-
-        if ($user_selector) {
-            $renderer = $PAGE->get_renderer('gradereport_user');
-            $showallusersoptions = true;
-            echo $renderer->graded_users_selector('user', $course, $userid, $currentgroup, $showallusersoptions);
-        }
 
         if ($currentgroup and !groups_is_member($currentgroup, $userid)) {
             echo $OUTPUT->notification(get_string('groupusernotmember', 'error'));
